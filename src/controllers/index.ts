ffc661532e242174b923b4f26acd874a37ee268a
Xapadoan <HollyolGamu@hotmail.com>
2024-04-02 14:11:31 +0200

    Rework staff and shops routers

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import recipesRouter from './recipes';
 import ingredientsRouter from './ingredients';
-import shopsRouter from './shops';
+import authRouter from './auth';
+import staffRouter from './staffs';
 
 const router = express.Router();
 
 router.use('/recipes', recipesRouter);
 router.use('/ingredients', ingredientsRouter);
-router.use('/shopsRouter', shopsRouter);
+router.use('/auth', authRouter);
+router.use('/staffs', staffRouter);
 router.use('*', (_, res) => res.status(404).json({ message: 'Not found' }));
 
 export default router;
---
+++ src/controllers/staffs/shops/readAll.ts
@@ -0,0 +1,16 @@
+import { StaffRepository } from '@repo/StaffRepository';
+import { Request, Response } from 'express';
+
+export async function readAll(req: Request, res: Response) {
+  try {
+    if (!req.staff) {
+      return res.status(401).json({ error: 'Missing Authentication' });
+    }
+    const staffRepo = new StaffRepository();
+    const shops = await staffRepo.readAllShops(req.staff.id);
+    return res.json(shops);
+  } catch (error) {
+    console.error('Failed to get me: ', error);
+    return res.status(500).json({ error: 'Unexpected server error' });
+  }
+}
