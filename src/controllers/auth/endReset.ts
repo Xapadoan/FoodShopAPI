534d9830e800a90b88d6d56ad94e60ef36b3ec5e
Xapadoan <HollyolGamu@hotmail.com>
2024-04-03 18:24:23 +0200

    Add validation for reset endpoints

--- src/controllers/auth/endReset.ts
+++ src/controllers/auth/endReset.ts
@@ -1,11 +1,34 @@
 import { Request, Response } from 'express';
 import authClient from '@lib/auth';
+import { HTTPError } from '@lib/http';
+import {
+  Failable,
+  ResetSessionSetupInput,
+  SessionSetupServerOutput,
+} from '@authservice/server';
 
-export async function endReset(req: Request, res: Response) {
+type ResetEndInput = Omit<ResetSessionSetupInput, 'userId'> & {
+  id: number;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function validate(body: any): body is ResetEndInput {
+  if (typeof body['id'] !== 'number') return false;
+  if (typeof body['EACResetToken'] !== 'string') return false;
+  return true;
+}
+
+export async function endReset(
+  req: Request,
+  res: Response<HTTPError | Failable<SessionSetupServerOutput>>
+) {
   try {
+    if (!validate(req.body)) {
+      return res.status(400).json({ error: 'Bad Request' });
+    }
     const { id, EACResetToken } = req.body;
     const session = await authClient().resetSetupSession({
-      userId: id,
+      userId: String(id),
       EACResetToken,
     });
     if (!session.success) {
--- src/controllers/auth/initReset.ts
+++ src/controllers/auth/initReset.ts
@@ -1,9 +1,28 @@
 import { Request, Response } from 'express';
 import authClient from '@lib/auth';
 import { StaffsRepository } from '@repo/StaffsRepository';
+import { HTTPError } from '@lib/http';
+import {
+  ResetInitServerOutput,
+  ResetInitServiceInput,
+} from '@authservice/server';
 
-export const initReset = async (req: Request, res: Response) => {
+type InitResetInput = ResetInitServiceInput;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function validate(body: any): body is InitResetInput {
+  if (typeof body['email'] !== 'string') return false;
+  return true;
+}
+
+export const initReset = async (
+  req: Request,
+  res: Response<HTTPError | (ResetInitServerOutput & { id: number })>
+) => {
   try {
+    if (!validate(req.body)) {
+      return res.status(400).json({ error: 'Bad Request' });
+    }
     const { email } = req.body;
     const staffRepo = new StaffsRepository();
     const staff = await staffRepo.read({ email });
@@ -14,7 +33,7 @@ export const initReset = async (req: Request, res: Response) => {
     if (!initResponse) {
       return res.status(500).json({ error: 'Failed to init reset' });
     }
-    return res.json({ ...initResponse, id: staff.id });
+    return res.json({ id: staff.id, ...initResponse });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: 'Unexpected error' });
