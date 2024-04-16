7c8a91f71ec010de951adb98ce955d33e67f8a72
Xapadoan <HollyolGamu@hotmail.com>
2024-04-16 17:37:51 +0200

    Create shop

--- src/controllers/staffs/shops/create.ts
+++ src/controllers/staffs/shops/create.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from 'express';
 import { Entry } from '@repo/Repository';
-import { Shop, Staff, ShopsRepository } from '@repo/ShopsRepository';
+import { Shop, ShopsRepository } from '@repo/ShopsRepository';
 import { HTTPError } from '@lib/http';
 
 export const create = async (
   req: Request,
-  res: Response<{ shop: Entry<Shop>; staff: Entry<Staff> } | HTTPError>
+  res: Response<HTTPError | Entry<Shop>>
 ) => {
   try {
     if (!req.staff) {
@@ -15,7 +15,9 @@ export const create = async (
     if (!repository.validate(req.body)) {
       return res.status(400).json({ error: 'Bad shop' });
     }
-    return res.status(201).json();
+    const shop = await repository.create(req.body);
+    await repository.addStaff(shop.id, req.staff.id);
+    return res.status(201).json(shop);
   } catch (err) {
     console.error(err);
     return res.status(500).json({ error: 'Unexpected server error' });
--- src/controllers/staffs/shops/index.ts
+++ src/controllers/staffs/shops/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { readAll } from './readAll';
+import { create } from './create';
 
 const router = express.Router({ mergeParams: true });
 
 router.get('/', readAll);
+router.post('/', create);
 
 export default router;
