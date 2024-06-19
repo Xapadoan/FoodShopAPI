7cc4a8f59098a04ee057622cfe30544579f56862
Xapadoan <HollyolGamu@hotmail.com>
2024-06-19 15:57:01 +0200

    Log error on initDb

--- src/data/index.ts
+++ src/data/index.ts
@@ -6,7 +6,10 @@ type Env = 'local' | 'development' | 'production' | 'test';
 const knex = Knex(config[(process.env.NODE_ENV as Env) || 'production']);
 
 export async function initDb() {
-  await knex.migrate.latest();
+  await knex.migrate.latest().catch((err) => {
+    console.error(err);
+    throw err;
+  });
 }
 
 export default knex;
